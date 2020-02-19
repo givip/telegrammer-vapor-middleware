8eb39144bd0ef83de6ba6ae9c7717dbaeb5e1069
givip <pataridzegivi@gmail.com>
2020-02-19 14:12:21 +0400

    Added restriction bu predefined path

--- Sources/TelegrammerMiddleware/TelegrammerMiddleware.swift
+++ Sources/TelegrammerMiddleware/TelegrammerMiddleware.swift
@@ -9,13 +9,19 @@ import Vapor
 import Telegrammer
 
 public class TelegrammerMiddleware: Middleware {
-    public var dispatcher: Dispatcher
+    public let dispatcher: Dispatcher
 
-    public init(dispatcher: Dispatcher) {
+    private let path: String
+
+    public init(path: String, dispatcher: Dispatcher) {
         self.dispatcher = dispatcher
+        self.path = path
     }
 
     public func respond(to request: Request, chainingTo next: Responder) -> EventLoopFuture<Response> {
+        guard request.url.path == "/\(path)" else {
+            return next.respond(to: request)
+        }
         guard let body = request.body.data else {
             request.logger.critical("Received empty request from Telegram Server")
             return next.respond(to: request)
